a6afc34e3a586e3757b07c9f6ad5edc103e3f3f6
Muhammad Shakeel <shakeel7521951@gmail.com>
2024-10-30 23:27:33 +0500

    second commit

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle
 import string
 import os
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
